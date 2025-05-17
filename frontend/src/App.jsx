e83ba4ca331e72bb33dce69fdb840e2a2d534f5e
hamza bouzian <bouzianh78@gmail.com>
2025-05-17 10:41:10 +0100

    Customizing THe home page design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import { Routes, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Signup from "./pages/Signup";
-import Signin from "./pages/Signin";
-import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
+// import Signup from "./pages/Signup";
+// import Signin from "./pages/Signin";
+// import Dashboard from "./pages/Dashboard";
+// import ProtectedRoute from "./components/ProtectedRoute";
 
 
 function App (){
@@ -11,16 +11,16 @@ function App (){
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/signin" element={<Signin/>}/>
-      <Route 
+      {/* <Route path="/signup" element={<Signup/>}/>
+      <Route path="/signin" element={<Signin/>}/> */}
+      {/* <Route 
         path="/dashboard"
         element={
           <ProtectedRoute>
             <Dashboard />
           </ProtectedRoute>
         }
-      />
+      /> */}
 
     </Routes>
   )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,26 @@
 import { Link } from 'react-router-dom';
 
 
+export default function Home (){
+
+    return (
+        <div className='min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 animate-gradient bg-size-200 px-4 sm:px-6 lg:px-8'>
+            <div className='bg-white bg-opacity-80 rounded-xl shadow-xl p-10 max-w-2xl w-full text-center'>
+                <h1 className='text-3xl sm:text-4xl md:text-5xl font-extrabold text-gray-800 mb-6'>
+                    Welcome to <span className='text-blue-600 mb-8'>Rbahi</span>
+                </h1>
+                <p className='text-base sm:text-lg md:text-xl text-gray-600 mb-8'>
+                    Your smart business to track, analyze, amin-h-screen flex items-center justify-center bg-gray-50 px-4 sm:px-6 lg:px-8nd grow with AI-powered insignts.
+                </p>
+
+                <Link
+                    to="/signup"
+                    className='inline-block bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg text-base sm:text-lg transition'>
+                        Get Started
+                    </Link>
+            </div>
+        </div>
+    );
+
+};
+
