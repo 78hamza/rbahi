a36197b49a56a93073d5e7b6813f9826598eb175
hamza bouzian <bouzianh78@gmail.com>
2025-05-20 19:04:29 +0100

    add successfully with the backend and the frontend for the file uploading and waiting now to add the fiel analysis in the next commits

--- backend/controllers/analysisController.js
+++ backend/controllers/analysisController.js
@@ -1,11 +1,15 @@
 const callPython = require('../services/callPython');
 const fs = require('fs');
-
+const path = require('path');
 exports.analyzeFile = async (req, res) => {
     try {
-        const filePath = req.file.path;
+        const filePath = path.join(__dirname, '..',req.file.path);
+        console.log('file path  is: ', filePath);
+
         const result = await callPython(filePath);
-        fs.unlinkSync(filePath);
+
+        fs.unlinkSync(filePath); // delete the file after processing
+
         res.json({ result });
     } catch (err) {
         console.error('analysis error', err);
--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -6,13 +6,13 @@ const analysisController = require('../controllers/analysisController');
 
 // Set up multer to store files in /uploads
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'uploads/'),
+    destination: (req, file, cb) => cb(null, 'uploads'),
     filename: (req, file, cb) => cb(null, Date.now() + path.extname(file.originalname))
 });
 
 const upload = multer({ storage });
 
 // POST route to upload a file and analyze it
-router.post('/upload', upload.single('file'), analysisController.analyzeFile);
+router.post('/uploads', upload.single('file'), analysisController.analyzeFile);
 
 module.exports = router;
